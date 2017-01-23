f8043c3009d4ffd288c35e5db541a9f518d7dab1
MeePwn <maybewaityou@gmail.com>
2017-01-23 11:19:11 +0800

    first commit

--- src/main/manager/DataManager.tsx
+++ src/main/manager/DataManager.tsx
@@ -2,7 +2,6 @@
  * Created by MeePwn
  * https://github.com/maybewaityou
  */
-import DebugUtility from '../utilities/function/DebugUtility';
 import DataUtility from '../utilities/data/DataUtility';
 import JSONUtility from '../utilities/data/JSONUtility';
 import NetworkUtility from '../utilities/data/NetworkUtility';
@@ -10,7 +9,6 @@ import StoreUtility from '../utilities/data/StoreUtility';
 
 const DataManager = {
 
-    ...DebugUtility,
     ...DataUtility,
     ...JSONUtility,
     ...NetworkUtility,
--- src/main/task/AtomicTask.tsx
+++ src/main/task/AtomicTask.tsx
@@ -6,20 +6,7 @@ import Just from '../context/Just';
 
 const AtomicTask = {
 
-    indexInfoTask() {
-        return Just.promiseTask('padAdapter', 'showIndexInfo', [])
-            .then((result) => {
-                return Just.promise(result);
-            }, (error) => {
-                return Just.promiseError(error);
-            });
-    },
-    validateTellerTask(userID) {
-        return Just.promiseTask('padAdapter', 'validateTeller', [userID])
-            .then((result) => {
-                return Just.promise(result.responseJSON.bsadata);
-            });
-    },
+
 
 };
 
