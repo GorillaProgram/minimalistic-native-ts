6ec6d3ae530a5302a6913d37187c67b9207132e1
MeePwn <maybewaityou@gmail.com>
2017-01-24 15:02:37 +0800

    first commit

--- src/main/components/view/ui/UIButton.tsx
+++ src/main/components/view/ui/UIButton.tsx
@@ -13,7 +13,6 @@ import {
 } from 'react-native';
 
 interface Props {
-    containerStyle?: React.ViewStyle,
     buttonStyle?: React.ViewStyle,
     textStyle?: React.TextStyle,
     handlePress?: () => void
@@ -26,32 +25,31 @@ interface State {
 export default class UIButton extends Component<Props, State> {
 
     render() {
-        console.log(this.props.handlePress);
         return (
-            <View style={[styles.container, this.props.containerStyle]}>
-                <TouchableOpacity style={this.props.buttonStyle} onPress={this.props.handlePress}>
-                    <Text style={[styles.text, this.props.textStyle]}>
-                        {this.props.children}
-                    </Text>
-                </TouchableOpacity>
-            </View>
+            <TouchableOpacity style={[styles.buttonStyle, this.props.buttonStyle]} onPress={this.props.handlePress}>
+                <Text style={[styles.text, this.props.textStyle]}>
+                    {this.props.children}
+                </Text>
+            </TouchableOpacity>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-        margin: 40,
+
+    buttonStyle: {
+        backgroundColor: "#999",
+        padding: 10,
+        borderRadius: 5,
     } as React.ViewStyle,
 
     text: {
         fontSize: 30,
+        color: 'white',
         fontWeight: '100',
         marginBottom: 20,
         textAlign: 'center',
+        backgroundColor: 'red'
     } as React.TextStyle,
 
 });
