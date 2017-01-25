52cdd26649554bf3b67d83782eeef7693ce4d4a9
MeePwn <maybewaityou@gmail.com>
2017-01-25 09:17:57 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@
  * https://github.com/maybewaityou
  */
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './dataflow/store/Store';
 import App from './App';
 
 interface Props {
@@ -17,7 +19,9 @@ export default class ReduxApp extends Component<Props, State> {
 
     render() {
         return (
-            <App />
+            <Provider store={configureStore()}>
+                <App />
+            </Provider>
         );
     }
 };
