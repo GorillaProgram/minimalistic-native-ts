8d4b9284f51acb8a254ad1edd2b5950cebd1d6f0
MeePwn <maybewaityou@gmail.com>
2017-02-04 19:47:48 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -54,10 +54,10 @@ class App extends Component<Props, State> {
                     dispatch(netWorkActionCreator('http://www.baidu.com', {
                         actionType: 'http://www.baidu.com'
                     }));
-                    Just.networkStatus()
-                        .then((networkStatus: boolean) => {
-                            Just.log('== networkStatus =====>>> ', networkStatus);
-                        });
+                    // Just.networkStatus()
+                    //     .then((networkStatus: boolean) => {
+                    //         Just.log('== networkStatus =====>>> ', networkStatus);
+                    //     });
                     // Just.isNetworkConnected()
                     //     .then((isNetworkConnected: boolean) => {
                     //         Just.log('== isNetworkConnected =====>>> ', isNetworkConnected);
